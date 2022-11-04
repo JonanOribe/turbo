26c7bb4f2bf7c7e2895225cdebc979cc2c89b789
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-11-04 10:39:52 +0100

    Base

---
+++ inventory-frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { Products } from "./components/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProductsCreate } from "./components/ProductsCreate";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/create" element={<ProductsCreate />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
